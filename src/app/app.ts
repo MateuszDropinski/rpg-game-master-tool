546ed899d38ece5e1d9c5cf338b693c420c926b6
Dominika Pacholec <dominika.pacholec@synergycodes.com>
2026-05-08 11:00:37 +0200

    Init diagram

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,29 @@
 import { Component } from '@angular/core';
+import {
+  initializeModel,
+  NgDiagramBackgroundComponent,
+  NgDiagramComponent,
+  provideNgDiagram,
+  type NgDiagramConfig,
+} from 'ng-diagram';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [NgDiagramComponent, NgDiagramBackgroundComponent],
+  providers: [provideNgDiagram()],
   templateUrl: './app.html',
+  styles: `
+    :host {
+      display: flex;
+      height: 100%;
+      width: 100%;
+    }
+  `,
 })
-export class App {}
+export class App {
+  model = initializeModel({ nodes: [], edges: [] });
+
+  config = {
+    background: { cellSize: { width: 20, height: 20 } },
+  } satisfies NgDiagramConfig;
+}
